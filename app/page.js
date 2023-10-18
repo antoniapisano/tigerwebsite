c2aad13694a356c6ebfff5e5cb1f30e8ac9f610f
antoniapisano <antonia.a.pisano@gmail.com>
2023-10-18 19:47:56 +0100

    need to add app font file

--- app/page.js
+++ app/page.js
@@ -1,7 +1,18 @@
 import Image from 'next/image'
+import { DM_Serif_Display } from 'next/font/google'
+
+const dm = DM_Serif_Display({
+  subsets: ['latin'],
+  weight: ['400'],
+  variable: "--font-dm",
+})
 
 export default function Home() {
   return (
-    <h1>I love Tigers!!!!!!!</h1>
+    <div className="Homewrap">
+      <h1>Tigers are the King of Cats</h1>
+        <h3 className={`${dm.variable} text-white`}> Help keep these magnificent beasts alive! </h3>
+          <button>Donate Now</button>
+    </div>
   )
 }
